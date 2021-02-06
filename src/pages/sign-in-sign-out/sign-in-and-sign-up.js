1a1f9003d320fe73eee055551e272356f4941ffd
Juan <pront23@gmail.com>
2021-02-06 17:14:19 +0900

    updated cart to use selectors

--- src/pages/sign-in-sign-out/sign-in-and-sign-up.js
+++ src/pages/sign-in-sign-out/sign-in-and-sign-up.js
@@ -3,9 +3,11 @@ import SignIn from '../../components/sign-in/sign-in';
 import SignUp from '../../components/sign-up/sign-up';
 import './sign-in-and-sign-up.styles.scss';
 
-export const SignInAndUpPage = () => (
+const SignInAndUpPage = () => (
   <div className="sign-in-and-sign-up">
     <SignIn />
     <SignUp />
   </div>
 );
+
+export default SignInAndUpPage;
