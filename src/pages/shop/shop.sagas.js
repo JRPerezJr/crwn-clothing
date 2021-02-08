84cfc2715c5c215b640797a49bd3695ee6199e55
Juan <pront23@gmail.com>
2021-02-08 14:34:28 +0900

    implemented and convert shop page to redux-saga

---
+++ src/pages/shop/shop.sagas.js
@@ -0,0 +1,34 @@
+import { call, put, takeEvery } from 'redux-saga/effects';
+
+import ShopActionTypes from '../../redux/shop/shop.types';
+
+import {
+  firestore,
+  convertCollectionsSnapshotToMap,
+} from '../../firebase/firebase.utils';
+
+import {
+  fetchCollectionsFailure,
+  fetchCollectionsSuccess,
+} from '../../redux/shop/shop.actions';
+
+export function* fetchCollectionsAsync() {
+  try {
+    const collectionRef = firestore.collection('collections');
+    const snapshot = yield collectionRef.get();
+    const collectionsMap = yield call(
+      convertCollectionsSnapshotToMap,
+      snapshot
+    );
+    yield put(fetchCollectionsSuccess(collectionsMap));
+  } catch (error) {
+    yield put(fetchCollectionsFailure(error.message));
+  }
+}
+
+export function* fetchCollectionsStart() {
+  yield takeEvery(
+    ShopActionTypes.FETCH_COLLECTIONS_START,
+    fetchCollectionsAsync
+  );
+}
