74bccad3c613dcd409dd95aae9255928a7a1453f
Juan <pront23@gmail.com>
2021-02-09 15:28:26 +0900

    amigrated front-end to client folder added express server

--- client/src/components/stripe-button/stripe-button.js
+++ client/src/components/stripe-button/stripe-button.js
@@ -2,13 +2,30 @@ import React from 'react';
 
 import StripeCheckout from 'react-stripe-checkout';
 
+import axios from 'axios';
+
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
   const publishableKey =
     'pk_test_51II3ByDmznwry1mPYdGoG7SKsYHzQG0HILJhVArXoopYfE9gvXXitoYaq4XwwffevsPRngXRuLAkVWOgoSVKevZD00gsRYz0Vv';
   const onToken = token => {
-    console.log(token);
-    alert('Payment Successful');
+    axios({
+      url: 'payment',
+      method: 'post',
+      data: {
+        amount: priceForStripe,
+        token,
+      },
+    })
+      .then(response => {
+        alert('Payment Successful');
+      })
+      .catch(error => {
+        console.log('Payment error: ', JSON.parse(error));
+        alert(
+          'There was an issue with your payment. Please be sure that you used the provided credit card information.'
+        );
+      });
   };
   return (
     <StripeCheckout
