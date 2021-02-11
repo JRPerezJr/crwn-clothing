a17e073809d4f73e35b4741d8e335d8e96cafef7
Juan <pront23@gmail.com>
2021-02-11 13:05:14 +0900

    updated servicejs file

--- server.js
+++ server.js
@@ -11,13 +11,13 @@ const stripe = require('stripe')(process.env.STRIPE_SEC_KEY);
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(compress());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(cors());
 
 if (process.env.NODE_ENV === 'production') {
+  app.use(compress());
   app.use(enforce.HTTPS({ trustProtoHeader: true }));
   app.use(express.static(path.join(__dirname, 'client/build')));
 
@@ -29,6 +29,10 @@ if (process.env.NODE_ENV === 'production') {
 app.listen(port, error => {
   if (error) throw error;
   console.log('Crown Clothing Server running on port ' + port);
+});
+
+app.get('/service-worker.js', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '..', 'build', 'service-worker.js'));
 });
 
 app.post('/payment', (req, res) => {
