8fd381d77ab887d423759d327f0c32dcdd11e648
Juan <pront23@gmail.com>
2021-02-11 12:39:51 +0900

    added compression to express server

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
+const compress = require('compression');
 
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 
@@ -10,12 +11,14 @@ const stripe = require('stripe')(process.env.STRIPE_SEC_KEY);
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(compress());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(cors());
 
 if (process.env.NODE_ENV === 'production') {
+  app.use(enforce.HTTPS({ trustProtoHeader: true }));
   app.use(express.static(path.join(__dirname, 'client/build')));
 
   app.get('*', function (req, res) {
